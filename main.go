9f223c44d329fdd4164a6274e25d5fc4f91eea04
Lucas <95702041+lukssantanna@users.noreply.github.com>
2021-12-10 09:30:44 -0300

    testing....

--- main.go
+++ main.go
@@ -4,7 +4,7 @@ import "net/http"
 
 func main(){
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request){
-		w.Write([]byte("<h1>Hello Night testing....</h1>"))
+		w.Write([]byte("<h1>Hello All night long....</h1>"))
 	})
 	http.ListenAndServe(":8080", nil)
 }
